85bba4cfc873c15afc98b5f5b0032e5e6a5332f0
Bouke Regnerus <bouke@regner.us>
2017-07-15 14:30:06 +0200

    Reading I2C data using struct.unpack

--- power_monitor.py
+++ power_monitor.py
@@ -10,32 +10,36 @@
 import smbus
 import struct
 import time
+from subprocess import call
 
 bus = smbus.SMBus(1)
-system_state = ""
+system_state = 0
 voltage = 0
 warn_voltage = 3.3
 shutdown_voltage = 3.2
 address = 0x04
 
 def configSlave(value):
-  # write some settings to the slave via I2C
-  # bus.write_byte(address, value)
-  return -1
+    # write some settings to the slave via I2C
+    # bus.write_byte(address, value)
+    return -1
 
-def getState():
-  system_state = "0000"
-  for i in range(len(system_state)):
-    system_state[i] += chr(bus.read_byte(address))
+def getState(block):
+    system_state = struct.unpack('<h', ''.join([chr(i) for i in block[0:2]]))[0]
+    print "State", system_state
 
-def compareVoltage():
-  # warn if warn_voltage reached
-  # shutdown shutdown_voltage reached
-  pass
+def compareVoltage(block):
+    voltage = struct.unpack('<f', ''.join([chr(i) for i in block[2:6]]))[0]
+    print "Voltage", voltage
+    
+    if voltage <= warn_voltage and voltage > shutdown_voltage:
+        pass # warn if warn_voltage reached
+    elif voltage <= shutdown_voltage:
+        call("sudo nohup shutdown -h now", shell=True) # shutdown shutdown_voltage reached
 
 while True:
-  time.sleep(1)
-  getState()
-  compareVoltage()
-
-  print "system_state contents: " + str(system_state)
+    time.sleep(1)
+    block = bus.read_i2c_block_data(address, 0)
+    
+    getState(block)
+    compareVoltage(block)
